3e6a8b74341b6e06f6eee80452e797fe96c14d7a
Andrei <andrei.evguenov@gmail.com>
2023-08-13 11:34:51 +1200

    Add and/or and groupled/neseted filters

--- src/dynamic_filters.rs
+++ src/dynamic_filters.rs
@@ -1,5 +1,5 @@
 /*
-    Adding Number, String and Bollean filter and filtering by three fields
+    Adding and/or and grouped/nested filter
 */
 
 use diesel::{connection::SimpleConnection, prelude::*, sql_types::Bool, sqlite::Sqlite};
@@ -27,42 +27,65 @@ enum StringFilter {
     Like(String),
 }
 
+enum AndOr {
+    And,
+    Or,
+}
+
 #[allow(non_camel_case_types)]
 enum Condition {
     number_field(NumberFilter),
     text_field(StringFilter),
     bool_field(bool),
+    And(Vec<Condition>),
+    Or(Vec<Condition>),
 }
 
 // Need this type for common condition expressions
 type BoxedCondition = Box<dyn BoxableExpression<test::dsl::test, Sqlite, SqlType = Bool>>;
 
-fn create_filter(conditions: Vec<Condition>) -> Option<BoxedCondition> {
+fn create_filter(conditions: Vec<Condition>, and_or: AndOr) -> Option<BoxedCondition> {
     conditions
         .into_iter()
         // Map into array of boxed conditions
-        .map::<BoxedCondition, _>(|condition| match condition {
-            Condition::number_field(f) => match f {
-                NumberFilter::Equal(value) => Box::new(test::dsl::number_field.eq(value)),
-                NumberFilter::NotEqual(value) => Box::new(test::dsl::number_field.ne(value)),
-                NumberFilter::GreaterThen(value) => Box::new(test::dsl::number_field.gt(value)),
-                NumberFilter::LowerThen(value) => Box::new(test::dsl::number_field.lt(value)),
-            },
-            Condition::text_field(f) => match f {
-                StringFilter::Equal(value) => Box::new(test::dsl::text_field.eq(value)),
-                StringFilter::NotEqual(value) => Box::new(test::dsl::text_field.ne(value)),
-                StringFilter::Like(value) => Box::new(test::dsl::text_field.like(value)),
-            },
-            Condition::bool_field(value) => Box::new(test::dsl::bool_field.eq(value)),
+        .filter_map::<BoxedCondition, _>(|condition| {
+            Some(match condition {
+                Condition::number_field(f) => match f {
+                    NumberFilter::Equal(value) => Box::new(test::dsl::number_field.eq(value)),
+                    NumberFilter::NotEqual(value) => Box::new(test::dsl::number_field.ne(value)),
+                    NumberFilter::GreaterThen(value) => Box::new(test::dsl::number_field.gt(value)),
+                    NumberFilter::LowerThen(value) => Box::new(test::dsl::number_field.lt(value)),
+                },
+                Condition::text_field(f) => match f {
+                    StringFilter::Equal(value) => Box::new(test::dsl::text_field.eq(value)),
+                    StringFilter::NotEqual(value) => Box::new(test::dsl::text_field.ne(value)),
+                    StringFilter::Like(value) => Box::new(test::dsl::text_field.like(value)),
+                },
+                Condition::bool_field(value) => Box::new(test::dsl::bool_field.eq(value)),
+                Condition::And(conditions) => match create_filter(conditions, AndOr::And) {
+                    Some(boxed_condition) => boxed_condition,
+                    None => return None,
+                },
+                Condition::Or(conditions) => match create_filter(conditions, AndOr::Or) {
+                    Some(boxed_condition) => boxed_condition,
+                    None => return None,
+                },
+            })
         })
         // Reduce to a boxed_condition1.and(boxed_condition2).and(boxed_condition3)...
-        .fold(
-            None,
-            |boxed_conditions, boxed_condition| match boxed_conditions {
-                Some(bc) => Some(Box::new(bc.and(boxed_condition))),
-                None => Some(boxed_condition),
-            },
-        )
+        .fold(None, |boxed_conditions, boxed_condition| {
+            Some(match boxed_conditions {
+                Some(bc) => match and_or {
+                    AndOr::And => Box::new(bc.and(boxed_condition)),
+                    AndOr::Or => Box::new(bc.or(boxed_condition)),
+                },
+                None => boxed_condition,
+            })
+        })
+}
+
+fn create__and_filter(conditions: Vec<Condition>) -> Option<BoxedCondition> {
+    create_filter(conditions, AndOr::And)
 }
 
 #[test]
@@ -97,7 +120,8 @@ fn test() {
         )
         .unwrap();
 
-    let condition = create_filter(vec![Condition::number_field(NumberFilter::Equal(1))]).unwrap();
+    let condition =
+        create__and_filter(vec![Condition::number_field(NumberFilter::Equal(1))]).unwrap();
     let result = vec!["1".to_string()];
 
     assert_eq!(
@@ -110,7 +134,7 @@ fn test() {
     );
 
     let condition =
-        create_filter(vec![Condition::number_field(NumberFilter::NotEqual(1))]).unwrap();
+        create__and_filter(vec![Condition::number_field(NumberFilter::NotEqual(1))]).unwrap();
     let result = vec!["2".to_string(), "3".to_string()];
 
     assert_eq!(
@@ -123,7 +147,7 @@ fn test() {
             .unwrap()
     );
 
-    let condition = create_filter(vec![
+    let condition = create__and_filter(vec![
         Condition::number_field(NumberFilter::GreaterThen(1)),
         Condition::number_field(NumberFilter::LowerThen(3)),
     ])
@@ -155,13 +179,77 @@ fn test() {
             .unwrap()
     );
 
-    let condition = create_filter(vec![
+    let condition = create__and_filter(vec![
         Condition::number_field(NumberFilter::GreaterThen(1)),
         Condition::text_field(StringFilter::Like("%4%".to_string())),
         Condition::bool_field(true),
     ])
     .unwrap();
     let result = vec!["4.2".to_string()];
+
+    assert_eq!(
+        result,
+        test::dsl::test
+            .filter(condition)
+            .select(test::dsl::id)
+            .load::<String>(&mut connection)
+            .unwrap()
+    );
+
+    // To test and/or and grouped/nested filter, we try this scenario
+    // when bool_field = true
+    // bool_field = true or (bool_field = true and bool_field = false) === true
+    // vs
+    // (bool_field = true or bool_field = true) and bool_field = false === false
+
+    connection
+        .batch_execute(
+            r#"
+            INSERT INTO test 
+              (id, number_field, bool_field) 
+            VALUES
+              ('5', 5, true);
+        "#,
+        )
+        .unwrap();
+
+    let condition = create__and_filter(vec![
+        Condition::number_field(NumberFilter::Equal(5)),
+        Condition::Or(vec![
+            Condition::bool_field(true),
+            Condition::And(vec![
+                Condition::bool_field(true),
+                Condition::bool_field(false),
+            ]),
+        ]),
+    ])
+    .unwrap();
+
+    let result = vec!["5".to_string()];
+
+    assert_eq!(
+        result,
+        test::dsl::test
+            .filter(condition)
+            .select(test::dsl::id)
+            .load::<String>(&mut connection)
+            .unwrap()
+    );
+
+    let condition = create__and_filter(vec![
+        Condition::number_field(NumberFilter::Equal(5)),
+        Condition::And(vec![
+            Condition::Or(vec![
+                Condition::bool_field(true),
+                Condition::bool_field(true),
+            ]),
+            Condition::bool_field(false),
+        ]),
+    ])
+    .unwrap();
+
+    // No return
+    let result: Vec<String> = vec![];
 
     assert_eq!(
         result,
